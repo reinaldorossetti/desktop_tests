45393996647c2e15ec62e77a1baf7a7df2c0a1f8
Reinaldo Rossetti <reinaldo.rossetti@outlook.com>
2018-02-10 14:59:47 -0200

    Add files via upload

---
+++ autoit_tests/exemplo01.py
@@ -0,0 +1,16 @@
+import autoit
+from time import sleep
+from expects import *
+
+autoit.run(filename="calc.exe")
+autoit.win_wait_active(title="Calculadora")
+autoit.send("3")
+autoit.send("*")
+autoit.send("3")
+autoit.send("=")
+sleep(2)
+text = autoit.win_get_text("Calculadora")
+print(text)
+
+assert "9" in text
+expect(text).to(contain("9"))
